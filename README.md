95adccf46ee5d463a74de917b121e0f9056b9809
ktruong12033@gmail.com <ktruong12033@gmail.com>
2013-06-17 19:12:17 -0400

    Content GAQ

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 Kikur
 =====
-### The source for funny images available for the Kik Messenger App
+### The source for funny images available on the Kik Messenger App
 
 View and share pictures with your friends and family through this card!
+
 All content belong to their respective content provider (Failblog, Memebase, Cheezburger, Imgur)
--- web/js/selector.js
+++ web/js/selector.js
@@ -2,18 +2,22 @@ App.populator('selector', function(page){
 	var p = $(page);
 
 	p.find('#fb').on('click', function(){
+		_gaq.push(['_trackEvent', 'PageOpen', 'Failblog']);
 		App.load('home', {content: 'Failblog', color: '#00AAFF'});
 	});
 
 	p.find('#mb').on('click', function(){
+		_gaq.push(['_trackEvent', 'PageOpen', 'Memebase']);
 		App.load('home', {content: 'Memebase', color: '#9ACD32'});
 	});
 
 	p.find('#cb').on('click', function(){
+		_gaq.push(['_trackEvent', 'PageOpen', 'Cheezburger']);
 		App.load('home', {content: 'Cheezburger', color: 'orange'});
 	});
 
 	p.find('#im').on('click', function(){
+		_gaq.push(['_trackEvent', 'PageOpen', 'Imgur']);
 		App.load('home', {content: 'Imgur', color: '#FF0099'});
 	});
 });
